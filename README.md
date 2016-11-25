3e9b325707ae8f3c0b120b447536075dadf5b8e1
ndueck <nolan.dueck@gmail.com>
2016-11-24 18:44:41 -0800

    added a bunch of content

--- README.md
+++
@@ -1,2 +0,0 @@
-# project1
-Brainstation Project Repository
--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,23 @@
-$( document ).ready(function() {
-  //Smooth Scroll Code
-  $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
-  });
+$(function() {
+    $('.navbar-collapse').click(function(){
+    	console.log('clicked!');
+    	$('.navbar').addClass('navbar-toggled');
+    });
 
-  //Swipebox Code
-  $( '.swipebox' ).swipebox();
+    $('.next-project').on('click', function(){
+    	window.location.href = 'indochino-detail.html';
+    }).on('mouseover', function(){
+    	$(this).css({
+    		'cursor' : 'pointer'
+    	});
+    });
+
+    $('.prev-project').on('click', function(){
+    	window.location.href = 'lululemon-detail.html';
+    }).on('mouseover', function(){
+    	$(this).css({
+    		'cursor' : 'pointer'
+    	});
+    });
 
 });
